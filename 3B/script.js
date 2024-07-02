3fb21c33ce01d2c9c49d94f4d385f409afc3cd8d
victoria323 <victoria.blanco@escola.pr.gov.br>
2024-07-01 22:44:27 -0300

    Add files via upload

--- 3B/script.js
+++ 3B/script.js
@@ -1,1 +1,24 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
+
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+
+const caixaAlternativas = document.querySelector(" .caixa-alternativas");
+
+const caixaResultado = document.querySelector(" .caixa-resultado");
+
+const textoResultado = document.querySelector(" .texto-resultado");
+
+const perguntas = [
+{
+    enunciado: "Insira o enunciado da pergunta 1",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+},
+{
+enunciado: "Insira o enunciado da Pergunta 2",
+alternativas: ["Alternativa 1", 'Alternativa 2'],
+},
+{
+enunciado: "Insira o enunciado da pergunta 3",
+    alternativas: ["Alternativa 1", "Alternativa 2"],
+},
+],
